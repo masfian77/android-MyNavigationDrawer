9fd12bc4c57b140af93683106864d9aa9dbe5f1c
ALFIAN SATRIA V.P <masfian77@gmail.com>
2019-09-16 17:16:22 +0700

    committed

--- app/src/main/java/com/example/mynavigationdrawer/MainActivity.java
+++ app/src/main/java/com/example/mynavigationdrawer/MainActivity.java
@@ -1,31 +1,21 @@
 package com.example.mynavigationdrawer;
 
 import android.os.Bundle;
-
 import com.bumptech.glide.Glide;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
-
 import android.view.MenuItem;
 import android.view.View;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
+import androidx.core.view.GravityCompat;
+import androidx.fragment.app.Fragment;
 import com.google.android.material.navigation.NavigationView;
-
 import androidx.drawerlayout.widget.DrawerLayout;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
 import android.view.Menu;
 import android.widget.Toast;
-
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
@@ -55,7 +45,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                             public void onClick(View view) {
                                 Toast.makeText(MainActivity.this, "Halo ini action dari snackbar", Toast.LENGTH_SHORT).show();
                             }
-                        }).show();
+                            }).show();
             }
         });
         drawer = findViewById(R.id.drawer_layout);
@@ -65,6 +55,14 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         Glide.with(MainActivity.this)
                 .load(profileImageUrl)
                 .into(profileCircleImageView);
+
+        if (savedInstanceState == null){
+            Fragment currentFragment = new HomeFragment();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.content_main,currentFragment)
+                    .commit();
+        }
 
     }
 
@@ -83,14 +81,74 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
 
     @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
+    @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        return false;
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        Bundle bundle = new Bundle();
+        Fragment fragment = null;
+        String title = "";
+        if (id == R.id.nav_home){
+            title = "Home";
+            fragment = new HomeFragment();
+        } else if (id == R.id.nav_camera) {
+            title = "Camera";
+            fragment = new PageFragment();
+            bundle.putString(PageFragment.EXTRAS,"Camera");
+            fragment.setArguments(bundle);
+        } else if (id == R.id.nav_gallery) {
+            title = "Gallery";
+            fragment = new PageFragment();
+            bundle.putString(PageFragment.EXTRAS,"Gallery");
+            fragment.setArguments(bundle);
+        } else if (id == R.id.nav_slideshow) {
+            title = "Slideshow";
+            fragment = new PageFragment();
+            bundle.putString(PageFragment.EXTRAS,"Slideshow");
+            fragment.setArguments(bundle);
+        } else if (id == R.id.nav_tools) {
+            title = "Tools";
+            fragment = new PageFragment();
+            bundle.putString(PageFragment.EXTRAS,"Tools");
+            fragment.setArguments(bundle);
+        } else if (id == R.id.nav_share) {
+            title = "Share";
+            fragment = new PageFragment();
+            bundle.putString(PageFragment.EXTRAS,"Share");
+            fragment.setArguments(bundle);
+        } else if (id == R.id.nav_send) {
+            title = "Send";
+            fragment = new PageFragment();
+            bundle.putString(PageFragment.EXTRAS,"Send");
+            fragment.setArguments(bundle);
+        }
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.content_main, fragment)
+                    .commit();
+        }
+
+        if (getSupportActionBar() != null)
+            getSupportActionBar().setTitle(title);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
